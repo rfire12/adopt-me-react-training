dd6a533c095418b789ffb66e8ffd3f8a70d77eac
Raul Paulino <example@example.com>
2019-10-24 13:50:08 -0400

    Using jsx

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
+const Pet = ({name, animal, breed }) => {
+  return  (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal }</h2>
+      <h2>{breed}</h2>
+    </div>
+  )
 };
 
 export default Pet;
